b3c89cbf0ee1f3643f5a7a7f72ad1255308c8a4a
najibullo98 <najibullokenjaev@gmail.com>
2024-04-19 19:37:22 +0500

    add test project

---
+++ Hamsafar.Tests/BasicMathsTest.cs
@@ -0,0 +1,35 @@
+using Hamsafar.Services;
+
+namespace Hamsafar.Tests;
+
+public class BasicMathsTest
+{
+    [Fact]
+    public void Test_AddMethod()
+    {
+        BasicMaths bm = new BasicMaths();
+        double res = bm.Add(10, 10);
+        Assert.Equal(res, 20);
+    }
+    [Fact]
+    public void Test_SubstractMethod()
+    {
+        BasicMaths bm = new BasicMaths();
+        double res = bm.Substract(10, 10);
+        Assert.Equal(res, 0);
+    }
+    [Fact]
+    public void Test_DivideMethod()
+    {
+        BasicMaths bm = new BasicMaths();
+        double res = bm.divide(10, 5);
+        Assert.Equal(res, 2);
+    }
+    [Fact]
+    public void Test_MultiplyMethod()
+    {
+        BasicMaths bm = new BasicMaths();
+        double res = bm.Multiply(10, 10);
+        Assert.Equal(res, 20);
+    }
+}
